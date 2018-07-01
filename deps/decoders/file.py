11df34e30751647f8326cda95def519791e91865
dm03514 <dm03514@gmail.com>
2018-07-01 14:13:48 -0400

    Service Test Harness (#1) (#3)
    * Service Test Harness
    
    * Building service test around application entry point
    
    Building service test around application entry point
    
    Service Test Harness
    
    Decoders Interface and Decoder in service test.  Decoder test stubs

---
+++ deps/decoders/file.py
@@ -0,0 +1,11 @@
+from deps.decoders.parsers import parse_dependency_str
+from . import DepsDecoder
+
+
+class FileLineDecoder(DepsDecoder):
+
+    def __init__(self, f):
+        self.f = f
+
+    def decode(self):
+        return [parse_dependency_str(line) for line in self.f]
---
+++ tests/unit/decoders/test_file_decoder.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class FileDecoderTestCase(unittest.TestCase):
+
+    @unittest.skip('TODO')
+    def test_decode_file_0_lines(self):
+        self.fail()
+
+    @unittest.skip('TODO')
+    def test_decode_file_1_line(self):
+        self.fail()
+
+    @unittest.skip('TODO')
+    def test_decode_file_multiple_lines(self):
+        self.fail()
